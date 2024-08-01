11d6f01f7973519ef153036ed4993f001d2df9a4
Esong <hamiddaphne38@gmail.com>
2024-08-01 15:21:31 +0100

    firstproject

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const searchInput=document.getElementById('searchInput');
+const searchButton=document.getElementById('searchButton');
+
+searchButton.addEventListener('click',()=>{
+    const searchTerm = searchInpu.value.toLowerCase();
+    //search logic
+    
+    console.log('Search term:',searchTerm);
+})
